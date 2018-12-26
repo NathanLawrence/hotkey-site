ec85bdaa3570c970f3a637d9921765adbe82bdcf
Nathan Lawrence <nathan@nathanlawrence.org>
2018-12-26 13:06:05 -0700

    Prettier configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,77 +1,78 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: "The Hotkey",
+    description: "Lorem ipsum"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
+        name: "uploads"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: 'uploads',
-            },
+              name: "uploads"
+            }
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-        ],
-      },
+              maxWidth: 2048
+            }
+          }
+        ]
+      }
     },
-	{
-	resolve: `gatsby-plugin-google-tagmanager`,
-	    options: {
-	      id: "GTM-NCTFMC9",
-
-	      // Include GTM in development.
-	      // Defaults to false meaning GTM will only be loaded in production.
-	      includeInDevelopment: false,
-
-	      // Specify optional GTM environment details.
-	      gtmAuth: "ZUK-PDkqyh1uZ53tcpxfVw",
-	      gtmPreview: "env-2",
-	    },
-	},
-	
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: `gatsby-plugin-google-tagmanager`,
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        id: "GTM-NCTFMC9",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // Specify optional GTM environment details.
+        gtmAuth: "ZUK-PDkqyh1uZ53tcpxfVw",
+        gtmPreview: "env-2"
+      }
     },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
+    },
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
+};
